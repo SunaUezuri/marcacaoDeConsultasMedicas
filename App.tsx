94ef0ab3576e6549ee6eeb68c514190337454b04
SunaUezuri <wesley.jane.santos@gmail.com>
2025-03-20 22:22:28 -0300

    Atualizando o app.tsx para carregar as rotas de navegação e a estilização

--- App.tsx
+++ App.tsx
@@ -1,33 +1,18 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
+import { ThemeProvider } from 'styled-components';
+import AppRoutes from './src/routes';
+import theme from './src/styles/theme';
+import { StatusBar } from 'react-native';
 
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content" backgroundColor="#007BFF"/>
-
-        <Stack.Navigator>
-          <Stack.Screen
-            name='Home'
-            component={HomeScreen}
-            options={{ headerShown: false}}
-          />
-        </Stack.Navigator>
-      </SafeAreaView>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" backgroundColor={theme.colors.primary}/>
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
