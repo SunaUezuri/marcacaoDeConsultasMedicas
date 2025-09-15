b7adf6d4b84e921a53e78e38db30ec8ce652d03e
SunaUezuri <wesley.jane.santos@gmail.com>
2025-09-15 18:48:25 -0300

    Refatorando a página de DoctorDashboardScreen para os bons padrões de Clean Code

---
+++ src/screens/DoctorDashboardScreen/types.ts
@@ -0,0 +1,15 @@
+import { Statistics } from "../../services/statistics";
+import { Appointment } from "../../types/appointments";
+
+export interface StatisticsDisplayProps {
+  statistics: Statistics | null;
+}
+
+export interface AppointmentCardItemProps {
+  appointment: Appointment;
+  onAction: (appointment: Appointment, action: 'confirm' | 'cancel') => void;
+}
+
+export interface StyledProps {
+  status: string;
+}
